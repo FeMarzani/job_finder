5eee62e7f6dedda16aab2937aeb3f923305fabc9
FeMarzani <marzaniarrieche@gmail.com>
2023-06-30 02:58:23 -0300

    :sparkles:feat: criando a rota para adicionar registros.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 const db = require('./db/connection');
+const bodyParser = require('body-parser');
+
 
 const PORT = 3000;
 
 app.listen(PORT, function() {
     console.log(`O Express está rodando na porta ${PORT}`);
 });
+
+// Corpo Parser (body parser)
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+
 
 // CONEXÃO DB
 db
@@ -24,3 +32,6 @@ db
 app.get('/', (req, res) => {
     res.send("Testando");
 })
+
+// Jobs routes
+app.use('/jobs', require('./routes/jobs'));
